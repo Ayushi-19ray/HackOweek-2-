9eea9d3d5f913c45852994a07af744cbe5097293
Ayushi-19ray <ayushiray1111@gmail.com>
2026-07-15 10:20:02 +0530

    Add files via upload

---
+++ src/utils/AppError.js
@@ -0,0 +1,16 @@
+/**
+ * Custom Application Error Class
+ * Extends native Error with HTTP status code support
+ */
+class AppError extends Error {
+  constructor(message, statusCode = 500, errors = null) {
+    super(message);
+    this.statusCode = statusCode;
+    this.errors = errors;
+    this.isOperational = true; // Distinguishes from programming errors
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = AppError;
